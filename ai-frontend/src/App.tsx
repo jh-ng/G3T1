17f3361493a5cac7af48d2e0e4642d2948f9469c
bentanj <bentanj@gmail.com>
2025-03-28 01:17:30 +0800

    AI front & backend
    barebones

---
+++ ai-frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TravelForm from './TravelForm';
+
+function App() {
+  return (
+    <div className="App">
+      <TravelForm />
+    </div>
+  );
+}
+
+export default App;
